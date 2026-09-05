ac81214d5ee192f0be48827d65f688fc82c71f06
SamArtz <219055717+doug-san@users.noreply.github.com>
2026-09-05 09:45:24 -0600

    Funcionando backend s3

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "adrian-terraform-state-lab3-2026"
+    key    = "devsecops/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
